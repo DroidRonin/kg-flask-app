93ae245f0c13e079ed3ad24e9783db12c495d80c
Jasvinder Singh <jasvinder2604@gmail.com>
2022-11-14 16:58:57 +0100

    Add files via upload

---
+++ Preprocess.py
@@ -0,0 +1,17 @@
+import nltk
+import regex as re
+
+
+class Preprocess(): 
+
+    def __init__(self, query):
+        self.query = query
+    def process(query):
+        sent = [each_string.lower() for each_string in query] 
+        sent  = [re.sub('[^a-zA-Z0-9\d\s]+', '', _) for _ in sent]
+        return sent
+
+
+
+
+ 
