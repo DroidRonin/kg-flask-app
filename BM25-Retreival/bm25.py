3ef95ca894f79e3089867e72b039615144b91a5b
Jasvinder Singh <jasvinder2604@gmail.com>
2022-11-14 17:25:16 +0100

    Update bm25.py

--- BM25-Retreival/bm25.py
+++ BM25-Retreival/bm25.py
@@ -3,8 +3,8 @@ from nltk.tokenize import word_tokenize
 from rank_bm25 import *
 
 def search_hits(query,data):
-    tokenized_query = [word_tokenize(i) for i in listofterms]    #tokenizing elements of input list
     data_list = data['tokenized_sents'].tolist()    #converting dataframe to list
+    tokenized_query = [word_tokenize(i) for i in query]    #tokenizing elements of input list
     bm25 = BM25Okapi(data_list)    #creating bm25 indexing
     docs = bm25.get_top_n(tokenized_query, data_list, n=5)     #finding top 5 relevant hits from the index
     df_search = data[data['tokenized_sents'].isin(docs)]       #mapping hits back to the main file
