39abfb114421ce638d24fde871b31083a6e7550b
Jasvinder Singh <jasvinder2604@gmail.com>
2022-11-14 16:58:12 +0100

    Add files via upload
    Uses word2vec pretrained model to find relevant triples

---
+++ CosineSimilarity.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from gensim.models import Word2Vec
+ 
+
+class CosineSimilarity():
+    def __init__(self, dataframe, concepts):
+        self.dataframe = dataframe
+        self.concepts = concepts
+    
+
+    def measure_similarity(self):
+        dataframe_min = pd.DataFrame(columns = ['source','edge','target'])   #Creating new dataframe which will contain the new results
+        model = Word2Vec.load("word2vec.model")  #Load pre-trained model here
+        max_score = 0
+        for i in self.dataframe['source']:
+            for j in concepts:
+                try:
+                        score = self.model.wv.similarity(i, j)   
+                        if score > max_score:
+                            max_score = score
+                            self.concepts = i
+                            dataframe_min = self.dataframe[self.dataframe['source'] == self.concepts]  #Appending the entries with highest score to the new dataframe
+                except:
+                        None
+        return dataframe_min
+
