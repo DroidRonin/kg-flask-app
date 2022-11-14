0fdd12142de4535f7dd86eb51a4f6e15325eb0c0
Jasvinder Singh <jasvinder2604@gmail.com>
2022-11-14 15:27:55 +0100

    Create bm25.py

---
+++ BM25-Retreival/bm25.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from rank_bm25 import *
+
+def search_hits(query,data):
+    tokenized_query = [word_tokenize(i) for i in listofterms]    #tokenizing elements of input list
+    data_list = data['tokenized_sents'].tolist()    #converting dataframe to list
+    bm25 = BM25Okapi(data_list)    #creating bm25 indexing
+    docs = bm25.get_top_n(tokenized_query, data_list, n=5)     #finding top 5 relevant hits from the index
+    df_search = data[data['tokenized_sents'].isin(docs)]       #mapping hits back to the main file
+    return df_search
+
+
+
+if __name__ == '__main__':
+    data = pd.read_csv("preprocessed.csv")    
+    query = ["reinforcement learning", "svm", "cnn", "convolutional neural network"]   #list of words to query
+    search = search_hits(query, data)                
+    search.to_csv("relevant_finds.csv")    
+
+
